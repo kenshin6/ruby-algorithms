971551fb2d4ee57968c38230870fdf4eca8a634f
Jeff <jeffreycgoldman@gmail.com>
2017-06-24 16:42:46 +0900

    Update my_nintendo_alert.rb

--- my_nintendo_alert.rb
+++ my_nintendo_alert.rb
@@ -1,6 +1,7 @@
 #Checks to see if the Switch is in stock on nintendo's Japanese store.
 #Unfortunately, I don't know what the page looks like when it's in stock,
 #so this simply sends a notification to your mac when SOLD OUT dissapears.
+#Feel free to use!
 
 require 'net/http'
 
